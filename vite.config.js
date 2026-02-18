a48eae1552b3eeb1335a39243f06b469c29f3f10
Gunawan92 <gunwp2@gmail.com>
2026-02-19 06:43:52 +0700

    Penambahan DevTOOLS Inspect element React

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { reactClickToComponent } from 'vite-plugin-react-click-to-component';
 
-export default defineConfig({
-  plugins: [react()],
-});
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react(),
+    mode === 'development' ? reactClickToComponent() : null,
+  ].filter(Boolean),
+}));
